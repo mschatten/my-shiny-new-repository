16f6e3443bd432aa61c2a46451135e5ea26173ba
dsabljic <domagoj.sabljicc@gmail.com>
2023-12-11 14:23:16 +0100

    Made changes to replaceat.py

--- replaceat.py
+++ replaceat.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
+import numpy as np
 
 for line in sys.stdin:
-    processed_line = line.strip().replace( '@', '_at_' )
-    print( processed_line )
+    processed_line = line.strip().replace('@', '_at_')
+    print(processed_line)
